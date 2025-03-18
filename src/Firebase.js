b10d991921fce5cfe40ca0da77bbd2452b659acc
Krispel Patrick Oliver Johann <Potestas06@users.noreply.github.com>
2025-03-18 08:16:21 +0100

    added firbase

---
+++ src/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3IiGtplFr6kJW2PGM2dIbqNejCLQ1N-E",
+  authDomain: "houseofblackjack.firebaseapp.com",
+  projectId: "houseofblackjack",
+  storageBucket: "houseofblackjack.firebasestorage.app",
+  messagingSenderId: "708299657346",
+  appId: "1:708299657346:web:7deab9f820344b9442e49b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export const auth = getAuth(app);
