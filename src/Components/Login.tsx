d3cf5cfab45c3ba18ab155e9dbf849eeee9edf97
Krispel Patrick Oliver Johann <Potestas06@users.noreply.github.com>
2025-03-18 16:48:09 +0100

    Added Login

---
+++ src/Components/Login.tsx
@@ -0,0 +1,71 @@
+// Login.tsx
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase";
+import MessageModal from "./MessageModal.tsx";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential.user);
+      setShowModal(true);
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 2000);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Ein unbekannter Fehler ist aufgetreten");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mb-3">
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="password">Passwort</label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <div className="alert alert-danger">{error}</div>}
+        <button type="submit" className="btn btn-primary w-100">
+          Login
+        </button>
+      </form>
+      {showModal && <MessageModal message="Login successful!" />}
+    </div>
+  );
+};
+
+export default Login;
