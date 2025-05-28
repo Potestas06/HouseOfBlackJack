e705cb5c36980e9ffb9e93ea85c5cfc9a24c3397
ogardoni <orell.gardoni@gvz.ch>
2025-05-28 15:00:15 +0200

    changes for ian

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, useRoutes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AuthPage from "./Pages/AuthPage.tsx";
 import ProtectedRoute from "./Components/MiddleWare.tsx";
 import HomePage from "./Pages/HomePage.tsx";
 
 import { auth } from "./Firebase";
-
-const AppRoutes = ({ user }) => {
-  const routes = useRoutes([
-    {
-      path: "/auth",
-      element: <AuthPage />,
-    },
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute user={user}>
-          <HomePage />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: "*",
-      element: (
-        <ProtectedRoute user={user}>
-          <HomePage />
-        </ProtectedRoute>
-      ),
-    },
-  ]);
-
-  return routes;
-};
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -52,7 +25,25 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <AppRoutes user={user} />
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute user={user}>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <ProtectedRoute user={user}>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
